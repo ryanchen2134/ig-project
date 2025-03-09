2250a9aa03060e4ea11ebd49fd0903bec45c112b
Michael <144758544+Whitoria@users.noreply.github.com>
2025-03-08 16:11:25 -0800

    Commit before checkout for new files

--- data_loader.py
+++ data_loader.py
@@ -1,25 +1,46 @@
+
+import os
 import torch
-import torchvision
-import numpy as np
-import math
-
+import clip
+import pandas as pd
+# from skimage import io, transform
+# import numpy as np
+# import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
 
-class SongDataset(Dataset):
+#Ignore Warnings
+# import warnings
+# warnings.filterwarnings("ignore")
+# plt.ion()
 
-    def __init__(self):
-        xy = np.loadtxt('./', delimiter=",", dtype=np.float32, skiprows=1)
-        self.x = torch.from_numpy(xy[:, 1:])
-        self.y = torch.from_numpy(xy[:, [0]]) # n_samples 1 
-        self.n_samples = xy.shape[0]
+from PIL import Image
+#Create Cystom Dataset Class
+class ImageMoodDataset(Dataset):
+    def __init__(self, file_path, root_dir, transform):
+        self.data = pd.read_csv(file_path, skiprows=1)
+        self.transform = transform
+        self.root_dir = root_dir
 
-    def __getitem__(self, index):
-        return self.x[index], self.y[index]
+        self.mood_to_idx = {mood: idx for idx, mood in enumerate(sorted(self.data.iloc[:, 1].unique()))}
+
+
     
     def __len__(self):
-        return self.n_samples
+        return len(self.data)
     
-dataset = SongDataset()
-first_data = dataset[0]
-features, labels = first_data
-print(features, labels)
+    def __getitem__(self, index):
+        if torch.is_tensor(index):
+            idx = idx.tolist()
+
+        img_name = os.path.join(self.root_dir, self.data.iloc[index, 0])
+        mood_label = self.data.iloc[index, 1]
+        
+        image = Image.open(img_name).convert('RGB')
+        image = self.transform(image)
+
+        mood_idx = self.mood_to_idx[mood_label]
+        mood_tensor = torch.tensor(mood_idx, dtype=torch.long)
+
+        return image, mood_tensor, img_name
+    
--- test.py
+++ test.py
@@ -1,30 +1,58 @@
 import clip
 import torch
-from PIL import Image
+import data_loader
+import numpy as np
+import torch.multiprocessing as mp
 
-# Load model
-model, preprocess = clip.load("ViT-B/32", device="cpu")
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+from sentence_transformers import SentenceTransformer
+from data_loader import ImageMoodDataset
+
+#Trying torch multiprocessing
+try:
+    mp.set_start_method('spawn', force= True)
+except RuntimeError:
+    pass
 
 # Load and preprocess image - This step resizes the images for the CLIP model
+device = "cuda" if torch.cuda.is_available() else "cpu"
+#Want to use ViT-L/32s
+model, preprocess = clip.load("ViT-B/32", device=device)
 
-# image = preprocess(Image.open(r"C:\Users\yumic\OneDrive\Pictures\CLIP test\white_doggo_1.jpg")).unsqueeze(0)
-# image = preprocess(Image.open(r"C:\Users\yumic\OneDrive\Pictures\CLIP test\happhy.jpg")).unsqueeze(0)
-image = preprocess(Image.open(r"C:\Users\yumic\projects\ig_porject\ig-project\ulzzang\0ba9e110ece390196abc1a79f6090d5d.jpg")).unsqueeze(0)
-# Tokenize text labels
-texts = clip.tokenize(["Happy", "Sad", "Energetic", "Calm", "2010's"])
+if __name__ == "__main__":
+    model.eval()
+    dataset = ImageMoodDataset(file_path='./image_data.csv', root_dir='./', transform=preprocess)
+    dataloader = DataLoader(dataset=dataset, batch_size=32, shuffle=True, num_workers=4)    
+    
+    #Tokenize
+    TOKENS = ["happy", "sad", "energetic", "calm", "2010's", "beauty", "female", "male", "japanese city pop", "city pop", "80's"]
+    texts = clip.tokenize(TOKENS).to(device)
 
-# Encode features
-with torch.no_grad():
-    image_features = model.encode_image(image)
-    text_features = model.encode_text(texts)
+    # Encode features
+    with torch.no_grad():
+        for batch_idx, (images, moods, file_paths) in enumerate(dataloader):
+            images = images.to(device)
+            
+            #Batch Number Message
+            print(f"\nProcessing Batch {batch_idx + 1}...")
 
-    # Normalize features to unit length
-    image_features /= image_features.norm(dim=-1, keepdim=True)
-    text_features /= text_features.norm(dim=-1, keepdim=True)
+            image_features = model.encode_image(images)
+            text_features = model.encode_text(texts)
 
-    # Compute similarity (cosine similarity)
-    similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+            # Normalize features to unit length
+            image_features /= image_features.norm(dim=-1, keepdim=True)
+            text_features /= text_features.norm(dim=-1, keepdim=True)
 
-# Convert to numpy and print
-probs = similarity.detach().cpu().numpy()
-print("Label Probabilities:", probs)
+            # Compute similarity (cosine similarity)
+            similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+
+            # Convert to numpy and print
+            probs = similarity.detach().cpu().numpy()
+            probs = np.round(probs, 2)
+
+            for img_idx, prob in enumerate(probs):
+                file_path = file_path[img_idx]
+                print(f"Image {img_idx +1}: {file_path}")
+                for token, p in zip(TOKENS, prob):
+                    print(f"Label: {token} [{float(p) * 100:.2f}%]")
